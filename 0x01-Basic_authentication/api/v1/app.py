e29691e953961765bc62216625b44e1ca9449f96
Peter Chibunna <peterchibunna@gmail.com>
2023-05-30 17:06:20 +0100

    Task #6: Basic Auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,6 +4,7 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
+from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.auth import Auth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -16,6 +17,8 @@ auth = None
 auth_type = getenv("AUTH_TYPE", None)
 if auth_type == "auth":
     auth = Auth()
+elif auth_type == "basic_auth":
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""The BasicAuth: class
+"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """Main BasicAuth that inherits from the Auth class
+    """
