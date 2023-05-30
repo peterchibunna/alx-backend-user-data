2d398c92aea5c4a34fd805ce848a05bc9f2076b2
Peter Chibunna <peterchibunna@gmail.com>
2023-05-30 17:18:21 +0100

    8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """The BasicAuth: class
 """
+import base64
+
 from .auth import Auth
 
 
@@ -8,7 +10,7 @@ class BasicAuth(Auth):
     """Main BasicAuth that inherits from the Auth class
     """
 
-    def extract_base64_authorization_header(self, authorization_header: str)\
+    def extract_base64_authorization_header(self, authorization_header: str) \
             -> str:
         """return the Base64 part of the authorization header for a Basic
         Authentication
@@ -24,3 +26,17 @@ class BasicAuth(Auth):
                 return authorization_header.split('Basic ')[1]
             except Exception:
                 return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """returns the value of the decoded base64 part
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header,
+                                    validate=True).decode('utf-8')
+        except Exception:
+            return None
