568b640ede53f86864a55e425a0670dde3b909d6
Peter Chibunna <peterchibunna@gmail.com>
2023-06-01 18:27:42 +0100

    3. User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -17,10 +17,15 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None) -> str:
         """Create a Session
         """
-        if user_id is None:
-            return None
-        if user_id is not None and type(user_id) != str:
+        if user_id is None or user_id is not None and type(user_id) != str:
             return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Returns a User ID based on a Session ID
+        """
+        if session_id is None or type(session_id) != str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
