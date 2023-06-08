590389e733b26115f2fdbe361bc9256f93bd2c0c
Peter Chibunna <peterchibunna@gmail.com>
2023-06-08 19:23:03 +0100

    10. Get session ID: Implement a session_id for a User instance

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -54,3 +54,22 @@ class Auth:
         except NoResultFound:
             return False
         return False
+
+    def create_session(self, email: str) -> str:
+        """10. Get session ID
+        In this task, you will implement the Auth.create_session method. It
+        takes an email string argument and returns the session ID as a string.
+        The method should find the user corresponding to the email, generate
+        a new UUID and store it in the database as the user’s session_id,
+        then return the session ID. Remember that only public methods of
+        self._db can be used.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is None:
+                return None
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
