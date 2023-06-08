056359ff91b2987dd778854af9f83e6df47499a7
Peter Chibunna <peterchibunna@gmail.com>
2023-06-08 14:57:58 +0100

    7. Register user function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,14 @@
 """Module:
 Flask Application
 """
+from typing import Any, Tuple
+
+from auth import Auth
 from flask import Flask, jsonify, request, abort, redirect
 
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -12,6 +17,19 @@ def bienvenue() -> str:
     """The home page
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def post_users() -> tuple[Any, int] | Any:
+    """Register user route
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
